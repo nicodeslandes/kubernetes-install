858ea5578f9d206c0d57210723b33fb21f6711a0
Nicolas Deslandes <ndeslandes@live.fr>
2022-10-22 01:16:08 +0100

    Fix script

--- kube_install.sh
+++ kube_install.sh
@@ -1,13 +1,14 @@
 # Install CRI-O
 sudo pacman -Sy cri-o crun
-sudo sed -i -E 's/^#\s*default_runtime.+/default_runtime = "crun"/' /etc/crio/crio.conf
-sudo cat << EOF > /etc/crio/crio.conf.d/01-crio-crun.conf
+sudo sed -i -E 's/^#\s*default_runtime = .+/default_runtime = "crun"/' /etc/crio/crio.conf
+cat << EOF > 01-crio-crun.conf
 [crio.runtime.runtimes.crun]
 runtime_path = "/usr/bin/crun"
 runtime_type = "oci"
 runtime_root = "/run/crun"
 EOF
 
+sudo mv 01-crio-crun.conf /etc/crio/crio.conf.d/
 # Start and enable crio.service
 sudo systemctl start crio
 sudo systemctl enable crio
